d28f9a2f332617eef7f9a2ac22b893e1e5b52b6e
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-03-05 00:50:36 +0600

    redux setup done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+
+  const state = useSelector((state) => state);
+  console.log(state)
 
   useEffect(() => {
     fetch('products.json')
       .then(res => res.json())
       .then(data => setProducts(data))
   }, [])
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       {
